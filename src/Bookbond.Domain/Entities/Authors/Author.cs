49409dbffd1290ab2c8bf6b0fa6fe770530471ac
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-07 16:23:14 +0500

    BookShelves created and some files modified

--- src/Bookbond.Domain/Entities/Authors/Author.cs
+++ src/Bookbond.Domain/Entities/Authors/Author.cs
@@ -1,12 +1,25 @@
 ﻿
 using Bookbond.Domain.Common;
+using Bookbond.Domain.Entities.Books;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bookbond.Domain.Entities.Authors;
 
 public class Author : Auditable
 {
+    [Required]
     public string FirstName { get; set; } = null!;
+    
+    [Required] 
     public string LastName { get; set; } = null!;
+    
+    [Required] 
     public string Biography { get; set; } = null!;
-
+    
+    public string? ProfilePicture { get; set; }
+    
+    [Required] 
+    public string LivingYears { get; set; } = null!;
+    
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
 }
---
+++ src/Bookbond.Domain/Entities/Bookshelves/Bookshelf.cs
@@ -0,0 +1,13 @@
+﻿
+using Bookbond.Domain.Common;
+using Bookbond.Domain.Entities.Users;
+
+namespace Bookbond.Domain.Entities.Bookshelves;
+
+public class Bookshelf : BaseEntity
+{
+    public long UserId { get; set; }
+    public virtual User User { get; set; } = null!;
+
+    public virtual ICollection<BookshelfBook> BookshelfBooks { get; set; } = new List<BookshelfBook>();
+}
