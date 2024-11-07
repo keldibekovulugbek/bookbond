76d76248f5474aa691b7be06709a3c2c40dd4ba8
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-07 16:27:13 +0500

    Categories added

---
+++ src/Bookbond.Domain/Entities/Categories/Category.cs
@@ -0,0 +1,14 @@
+﻿
+using Bookbond.Domain.Common;
+using Bookbond.Domain.Entities.Books;
+
+namespace Bookbond.Domain.Entities.Categories;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+    
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
