9a8f2ceb0cb2d5bbfb9230abe2bce358293cd685
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-08 16:32:18 +0500

    add dataconfiguration and add migration

---
+++ src/Bookbond.Api/Configurations/LayerConfigurations/DataAccessConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Bookbond.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using EFCore.NamingConventions;
+
+namespace Bookbond.Api.Configurations.LayerConfigurations
+{
+    public static class DataAccessConfiguration
+    {
+        public static void Configure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("BookbondDbConnection")).UseSnakeCaseNamingConvention();
+            });
+        }
+    }
+}
