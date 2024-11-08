6ea0bf8daffca14695802efec267a3fc99398ac1
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-08 17:32:25 +0500

    Added IUnitOfWork

---
+++ src/Bookbond.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿
+using Bookbond.DataAccess.Interfaces.Authors;
+using Bookbond.DataAccess.Interfaces.Books;
+using Bookbond.DataAccess.Interfaces.Bookshelves;
+using Bookbond.DataAccess.Interfaces.Categories;
+using Bookbond.DataAccess.Interfaces.Reviews;
+using Bookbond.DataAccess.Interfaces.Users;
+
+namespace Bookbond.DataAccess.Interfaces;
+
+public interface IUnitOfWork
+{
+    IAuthorRepository Authors { get; }
+
+    IBookRepository Books { get; }
+
+    IBookshelfRepository Bookshelves { get; }
+    IBookshelfBookRepository BookshelfBooks { get; }
+
+    ICategoryRepository Categories { get; }
+    ICategoryBookRepository CategoryBooks { get; }
+
+    IReviewRepository Reviews { get; }
+
+    IUserRepository Users { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+
+}
