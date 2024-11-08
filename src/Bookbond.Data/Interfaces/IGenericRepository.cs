3abc940c7611fb67a70eee25554ef27a47463912
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-08 17:14:14 +0500

    added repository

---
+++ src/Bookbond.Data/Interfaces/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bookbond.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookbond.DataAccess.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> GetByIdAsync(long id);
+
+    IQueryable<T> GetAll();
+    
+    IQueryable<T> Where(Expression<Func<T,bool>> expression);
+
+    Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> expression);
+
+    void Add(T entity);
+
+    Task<T> Update(long id,T entity);
+
+    Task Delete(long id);
+}
