626649fb0728ffd283c9ccee32e9066d110cafda
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-11 16:34:14 +0500

    added Repository methods

--- src/Bookbond.Data/Interfaces/IGenericRepository.cs
+++ src/Bookbond.Data/Interfaces/IGenericRepository.cs
@@ -10,17 +10,17 @@ namespace Bookbond.DataAccess.Interfaces;
 
 public interface IGenericRepository<T> where T : BaseEntity
 {
-    Task<T> GetByIdAsync(long id);
+    Task<T?> GetByIdAsync(long id);
 
     IQueryable<T> GetAll();
     
     IQueryable<T> Where(Expression<Func<T,bool>> expression);
 
-    Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> expression);
+    Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> expression);
 
     void Add(T entity);
 
-    Task<T> Update(long id,T entity);
+    void Update(long id,T entity);
 
-    Task Delete(long id);
+    void Delete(long id);
 }
