3ea5a9d43396dabd653e71d30c547cb1e0dadb2a
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-21 19:54:39 +0500

    AccountCreateDTO created

---
+++ src/Bookbond.Service/DTOs/Accounts/AccountCreateDTO.cs
@@ -0,0 +1,32 @@
+﻿
+using Bookbond.Domain.Entities.Users;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookbond.Service.DTOs.Accounts;
+
+public class AccountCreateDTO
+{
+
+    [Required, MinLength(3)]
+    public string Firstname { get; set; } = string.Empty;
+
+    [Required, MinLength(3)]
+    public string Lastname { get; set; } = string.Empty;
+
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+
+    public static implicit operator User(AccountCreateDTO accountCreateDto)
+    {
+        return new User()
+        {
+            FirstName = accountCreateDto.Firstname,
+            LastName = accountCreateDto.Lastname,
+            Email = accountCreateDto.Email,
+        };
+    }
+}
